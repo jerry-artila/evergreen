77252c76f45de92a480d5c85571f6c74fdda4fcf
jerry-artila <jerry.artila@gmail.com>
2025-08-20 15:48:54 +0800

    update

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ const AVAILABLE_FONT_SIZES = [
   // 'text-sm',      // 14px
   // 'text-base',    // 16px
   // 'text-lg',      // 18px
-  'text-xl',      // 20px
+  // 'text-xl',      // 20px
   'text-2xl',     // 24px
   'text-3xl',     // 30px
   'text-4xl',     // 36px (預設)
